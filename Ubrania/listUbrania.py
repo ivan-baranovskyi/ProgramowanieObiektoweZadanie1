66d78ad2a6a04b1dbb396b149d32902b702ef1df
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-03-13 12:43:32 +0100

    List of clothes

---
+++ Ubrania/listUbrania.py
@@ -0,0 +1,56 @@
+from ubrania import Ubrania
+
+class listOfClotes (Ubrania):
+    def __init__(self, name, price):
+        super().__init__(name, price)
+
+skarpetki =  listOfClotes('skarpetki', '10zł')
+skarpetki.make_noise()
+
+buty =  listOfClotes('buty', '300zł')
+buty.make_noise()
+
+chustka =  listOfClotes('chustka', '12zł')
+chustka.make_noise()
+
+chusta =  listOfClotes('chusta', '10zł')
+chusta.make_noise()
+
+czapka =  listOfClotes('czapka', '80zł')
+czapka.make_noise()
+
+rękawiczki =  listOfClotes('rękawiczki', '10zł')
+rękawiczki.make_noise()
+
+spodnie =  listOfClotes('spodnie', '100zł')
+spodnie.make_noise()
+
+sukienka =  listOfClotes('sukienka', '100zł')
+sukienka.make_noise()
+
+spódnica =  listOfClotes('spódnica', '50zł')
+spódnica.make_noise()
+
+płaszcz =  listOfClotes('płaszcz', '180zł')
+płaszcz.make_noise()
+
+koszula =  listOfClotes('koszula', '200zł')
+koszula.make_noise()
+
+podkoszulka =  listOfClotes('podkoszulka', '100zł')
+podkoszulka.make_noise()
+
+okulary =  listOfClotes('okulary', '100zł')
+okulary.make_noise()
+
+opaskaNaGłowę =  listOfClotes('opaskaNaGłowę', '90zł')
+opaskaNaGłowę.make_noise()
+
+szal =  listOfClotes('szal', '80zł')
+szal.make_noise()
+
+bluza =  listOfClotes('bluza', '110zł')
+bluza.make_noise()
+
+sweter =  listOfClotes('sweter', '120zł')
+sweter.make_noise()
---
+++ Ubrania/ubrania.py
@@ -0,0 +1,8 @@
+class Ubrania:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def make_noise(self):
+        print('Name of clothes: ', self.name)
+        print('!Price!: ', self.price)
